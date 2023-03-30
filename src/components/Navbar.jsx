e849269612677ec193b37ec5a6f23c4e522684f7
pwtansg <pwtan.edu@gmail.com>
2023-03-30 19:36:29 +0800

    initial commit with Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+import { AiOutlineMenu, AiOutlineSearch, AiFillTag } from 'react-icons/ai'
+import { BsCart3 } from 'react-icons/bs'
+import { FaTimes, FaTruck, FaWallet } from 'react-icons/fa'
+import { MdHelp, MdFavorite } from 'react-icons/md'
+
+const Navbar = () => {
+
+    const [showSidebar, setShowSidebar] = useState(false)
+    return (
+        <div className='max-w-[1640] mx-auto flex justify-between items-center p-4 bg-[#f8f8f8]'>
+            {/* left side items  */}
+            <div className='flex items-center'>
+                <div className='cursor-pointer'>
+                    <AiOutlineMenu size={30} onClick={() => setShowSidebar(!showSidebar)} />
+                </div>
+                <h1 className='text-2xl sm:text-3xl lg:text-4xl p-2'>
+                    Big <span className='font-bold font-mono'>Store</span>
+                </h1>
+                <div className='hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]'>
+                    <p className='bg-black text-white rounded-full p-2'>Delivery</p>
+                    <p className='p-2'>Pickup</p>
+                </div>
+            </div>
+
+
+            {/* Search Input */}
+            <div className='flex items-center'>
+                <div className='bg-gray-200 rounded-full flex items-center px-2 w-[140px] sm:w-[300px]'>
+                    <AiOutlineSearch size={25} />
+                    <input
+                        className='bg-transparent p-2 w-full focus:outline-none'
+                        type='text'
+                        placeholder='Search...'
+                    />
+                </div>
+                <button className='bg-black text-white p-2 ml-[5px]'><BsCart3 size={20} /></button>
+            </div>
+
+            {/* overlay */}
+            {showSidebar && <div className='bg-black/80 fixed inset-0'></div>}
+
+            {/* side bar menu */}
+            {showSidebar &&
+                <div className='fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300'>
+                    <FaTimes size={20} className='absolute right-4 mt-4 cursor-pointer' onClick={()=>setShowSidebar(!showSidebar)}/>
+                    <h2 className='text-2xl p-4'>Menu</h2>
+                    <nav>
+                        <ul className='flex-flex-col p-4 text-gray-700'>
+                            <li className='text-xl py-3 flex items-center' ><FaTruck size={25} className='mr-4' />Orders</li>
+                            <li className='text-xl py-3 flex items-center' ><MdFavorite size={25} className='mr-4' />Favorites</li>
+                            <li className='text-xl py-3 flex items-center' ><FaWallet size={25} className='mr-4' />Wallet</li>
+                            <li className='text-xl py-3 flex items-center' ><MdHelp size={25} className='mr-4' />Help</li>
+                            <li className='text-xl py-3 flex items-center' ><AiFillTag size={25} className='mr-4' />Promotions</li>
+                        </ul>
+                    </nav>
+                </div>
+            }
+        </div>
+    )
+}
+
+export default Navbar
