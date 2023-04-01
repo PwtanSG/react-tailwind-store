10b4f5b1ad2174f22d3b130b8f5e284c38634647
pwtansg <pwtan.edu@gmail.com>
2023-04-01 16:41:21 +0800

    Move ProductDetails to pages dir

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import ProductDetail from './components/ProductDetail'
+import ProductDetail from './pages/ProductDetail'
 function App() {
   return (
     <>
