87049f4e389bf98eca0bd915844ffb74185deb07
pwtansg <pwtan.edu@gmail.com>
2023-05-27 18:58:13 +0800

    Add prod build and deploy script

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='mt-auto bg-[#f4f4f4] py-8 text-center text-gray-600'>
-        Copyright 2023. All rights reserved.
+        Copyright 2023. All rights reserved. TPW
     </footer>
   )
 }
